71be298aa5c49f2c10bd16ce74204695f6bcff7b
chikwongyip <527439024@qq.com>
2023-10-10 07:28:20 +0800

    header with search

--- src/components/common/IndexHeader.vue
+++ src/components/common/IndexHeader.vue
@@ -1,8 +1,28 @@
 <template>
     <b-container>
         <b-row>
-          <b-col>1 of 3</b-col>
-          <b-col>2 of 3</b-col>
+          <b-col>
+            <div>
+              <b-img :src="company.logo" fluid alt="没有找到图片">
+
+              </b-img> 
+            </div>
+          </b-col>
+          <b-col cols="8">
+            <div>
+              <b-input-group>
+                <b-form-select v-model="selected" 
+                :options="options" 
+                value-field="item" 
+                text-field="name"
+                class="w-10 p-1"
+                > 
+                </b-form-select> 
+                <b-form-input aria-label="Input" class="w-50 p-3"></b-form-input>
+                <b-button variant="outline-primary">搜索</b-button>
+              </b-input-group>
+            </div>
+          </b-col>
           <b-col>3 of 3</b-col>
         </b-row> 
       </b-container>
@@ -15,7 +35,13 @@
         return{
           company:{},
           brand:[],
-          category:[]
+          category:[],
+          selected:'1',
+          options:[
+            { item:'1', name:"品牌"},
+            { item:'2', name:"类别"},
+            { item:'3', name:"名称"},
+          ]
         }
       },
       methods:{
