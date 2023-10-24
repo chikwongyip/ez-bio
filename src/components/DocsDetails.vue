8644ed70a4fcfb70bce3c38bfc8ce390b87786a0
chikwongyip <527439024@qq.com>
2023-10-25 07:19:04 +0800

    header layout

---
+++ src/components/DocsDetails.vue
@@ -0,0 +1,24 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { getDocs } from "@/api";
+export default {
+  data() {
+    return {
+      data: []
+    };
+  },
+  methods: {
+    getData() {
+      getDocs().then((res) => {
+        this.data = res.data;
+      });
+    }
+  },
+  mounted() {
+    this.getData();
+  }
+};
+</script>
+<style scoped></style>
--- src/components/common/IndexHeader.vue
+++ src/components/common/IndexHeader.vue
@@ -1,11 +1,13 @@
 <template>
-  <header>
-    <div>
-      <router-link :to="{ name: 'index' }">
+  <header class="container">
+    <nav class="navbar navbar-expand-md navbar-light justify-content-center">
+      <router-link
+        :to="{ name: 'index' }"
+        class="navbar-brand"
+        style="width: 300px"
+      >
         <b-img :src="company.logo" fluid alt="没有找到图片"></b-img>
       </router-link>
-    </div>
-    <nav class="navbar navbar-expand-md bg-light navbar-light">
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <b-nav tabs fill>
           <b-nav-item-dropdown toggle-class="text-dark" text="品牌">
@@ -35,110 +37,33 @@
           <b-nav-item>技术文档</b-nav-item>
           <b-nav-item>关于我们</b-nav-item>
         </b-nav>
-        <form class="form-inline my-2 my-lg-0">
-          <b-form-select
-            v-model="selected"
-            :options="options"
-            value-field="item"
-            text-field="name"
-            class="w-15"
-          >
-          </b-form-select>
-          <input
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button
-            class="btn btn-outline-success my-2 my-sm-0"
-            type="submit"
-            @click="submitSearch"
-          >
-            Search
-          </button>
-        </form>
       </div>
+      <form class="form-inline">
+        <b-form-select
+          v-model="selected"
+          :options="options"
+          value-field="item"
+          text-field="name"
+          class="w-15"
+        >
+        </b-form-select>
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          v-mode="inputText"
+        />
+        <button
+          class="btn btn-outline-success my-2 my-sm-0"
+          type="submit"
+          @click="submitSearch"
+        >
+          Search
+        </button>
+      </form>
     </nav>
   </header>
-  <!-- <b-container>
-    <b-row align-v="center">
-      <b-col cols="mb-4">
-        <div>
-          <router-link :to="{ name: 'index' }">
-            <b-img :src="company.logo" fluid alt="没有找到图片"></b-img>
-          </router-link>
-        </div>
-      </b-col>
-      <b-col class="mb-2">
-        <div>
-          <p class="h1 mb-2">
-            <b-icon icon="telephone-fill"></b-icon> {{ company.tel }}
-          </p>
-        </div>
-      </b-col>
-    </b-row>
-    <b-row align-v="center">
-      <b-col>
-        <div>
-          <b-input-group>
-            <b-form-select
-              v-model="selected"
-              :options="options"
-              value-field="item"
-              text-field="name"
-              class="w-15 p-1"
-            >
-            </b-form-select>
-            <b-form-input
-              aria-label="Input"
-              class="w-50 p-3"
-              v-model="inputText"
-            ></b-form-input>
-            <b-button variant="outline-primary" @click="submitSearch"
-              >搜索</b-button
-            >
-          </b-input-group>
-        </div>
-      </b-col>
-    </b-row>
-    <div class="mt-4"></div>
-    <b-row>
-      <b-col>
-        <div>
-          <b-nav tabs fill>
-            <b-nav-item-dropdown text="品牌">
-              <b-dropdown-item v-for="item in brand" :key="item.brand_id">
-                <router-link
-                  :to="{
-                    name: 'productBrand',
-                    params: { brand_id: item.brand_id }
-                  }"
-                >
-                  {{ item.brand_name }}
-                </router-link>
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item-dropdown text="类型">
-              <b-dropdown-item v-for="item in category" :key="item.category_id">
-                <router-link
-                  :to="{
-                    name: 'productCategory',
-                    params: { category_id: item.category_id }
-                  }"
-                >
-                  {{ item.category_name }}
-                </router-link>
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-            <b-nav-item>技术文档</b-nav-item>
-            <b-nav-item>关于我们</b-nav-item>
-          </b-nav>
-        </div>
-      </b-col>
-    </b-row>
-    <div class="mt-4"></div>
-  </b-container> -->
 </template>
 <script>
 import { companyInfo, getBrand, getCategory } from "../../api/index";
