7747e4ecf92d6f2476d5b4885c0da4f31b77c113
chikwongyip <527439024@qq.com>
2023-10-30 07:45:18 +0800

    task

--- src/main.js
+++ src/main.js
@@ -1,25 +1,27 @@
-import Vue from 'vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import VueRouter from "vue-router"
-import routes from "../src/router/router"
-import VueCookies from 'vue-cookies'
-import App from './App.vue'
+import Vue from "vue";
+import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
+import VueRouter from "vue-router";
+import routes from "../src/router/router";
+import VueCookies from "vue-cookies";
+import App from "./App.vue";
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-Vue.use(VueRouter)
-Vue.use(VueCookies, { expires: '7d' })
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+Vue.use(VueRouter);
+Vue.use(VueCookies, { expires: "7d" });
+Vue.config.productionTip = false;
 
-const router = new VueRouter(
-  {
-    mode: "history",
-    routes: routes
-  }
-)
+const router = new VueRouter({
+  mode: "history",
+  routes: routes
+});
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
   router
-}).$mount('#app')
+}).$mount("#app");
+
+//to-do:favoricon
+// aboutus
+// admin
